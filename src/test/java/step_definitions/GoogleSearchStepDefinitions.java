a5661c9f4102ccddf8dba6c012bdec8707e849c0
ShokhbozUsmanov <tashcity@mail.ru>
2022-10-25 23:48:48 +0500

    Upd

--- src/test/java/step_definitions/GoogleSearchStepDefinitions.java
+++
@@ -1,8 +0,0 @@
-package step_definitions;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
-public class GoogleSearchStepDefinitions {
-}
---
+++ src/test/java/step_definitions/GoogleSearch_StepDefinitions.java
@@ -0,0 +1,17 @@
+package step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleSearch_StepDefinitions {
+    @Given ("user is on Google search page")
+    public void user_is_on_google_search_page(){
+    }
+    @When("user types apple in the google search box and click enter")
+    public void userTypesAppleInTheGoogleSearchBoxAndClickEnter() {
+    }
+    @Then("user sees apple - Google Search is in the google title")
+    public void userSeesAppleGoogleSearchIsInTheGoogleTitle() {
+    }
+}
