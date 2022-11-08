a6cfc1af1df2eb22da98e48828982fc4dc10ebf4
ShokhbozUsmanov <tashcity@mail.ru>
2022-11-08 22:16:21 +0500

    json file report

--- src/test/java/runner/CukesRunner.java
+++ src/test/java/runner/CukesRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber/cucumber.json"
         },
         features = "src/test/resources/features",
         glue = "step_definitions",
